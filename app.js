5efda9ae6cfc92b8d17f77503733bd6583ec78f2
Victor Carpenter <victorcarpenter123@gmail.com>
2017-03-01 15:39:37 -0500

    got the basic look done

---
+++ app.js
@@ -0,0 +1,10 @@
+var btn = document.querySelector('.searchBtn'),
+    input = document.querySelector('.searchBox');
+console.log(btn,input);
+
+
+btn.addEventListener('click',(evt)=>{
+  if(input.value.length !== 0){
+    
+  }
+})
