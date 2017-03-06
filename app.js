4b50269e16766cf024b786606339160d10263c42
Victor Carpenter <victorcarpenter123@gmail.com>
2017-03-06 09:40:14 -0500

    got the if to see numberds

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var btn = document.querySelector('.searchBtn'),
-    input = document.querySelector('.searchBox');
-console.log(btn,input);
+    input = document.querySelector('.searchBox'),
+    text = document.querySelector('.instructions');
+// console.log(btn,input);
 
 
 btn.addEventListener('click',(evt)=>{
   if(input.value.length !== 0){
-    
+    if(isNaN(input.value) !== true){
+      let num = input.value
+      
+    }
   }
 })
